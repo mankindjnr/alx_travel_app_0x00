753378d4e5ac2dc4ee26f98bbcff959794aa9d0c
mankindjnr <kamos8646@stu.kemu.ac.ke>
2025-01-19 19:18:58 +0300

    milestone

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(ModelViewSet):
+    """
+    API endpoint for managing Listings.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(ModelViewSet):
+    """
+    API endpoint for managing Bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
